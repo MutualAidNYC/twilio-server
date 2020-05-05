56aeb1f6484a7c17016de4deb464a8b1c9691f59
Aaron Young <brainomite@gmail.com>
2020-05-05 09:08:22 -0400

    style refactor: Apply initial linting
    1. Ran eslint and addressed all issues
    2. Ran prettier and addressed all issues

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-"use stict";
-console.log("warming up");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config(); // load the local .env file
+'use stict';
+
+console.log('warming up');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config(); // load the local .env file
 }
-const app = require("./server");
+const app = require('./server');
 const {
   getInitialShift,
   saveShiftNumbers,
-} = require("./workers/getShiftNumbers");
+} = require('./workers/getShiftNumbers');
+require('./routes/api/schedule');
+require('./routes/api/simuldial');
 
-require("./routes/api/schedule");
-require("./routes/api/simuldial");
+const port = process.env.PORT || 80;
 
+const onListen = () => console.log(`Server is running on port ${port}`);
 const startUp = (languagesToPhones) => {
   saveShiftNumbers(languagesToPhones);
   app.listen(port, onListen);
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
 });
-
-const port = process.env.PORT || 80;
-const onListen = () => console.log(`Server is running on port ${port}`);
 
 // start the engine
 getInitialShift(startUp);
--- workers/getShiftNumbers.js
+++ workers/getShiftNumbers.js
@@ -1,21 +1,28 @@
-"use strict";
-const moment = require("moment");
-const app = require("../server");
+const dotenv = require('dotenv');
+const moment = require('moment');
+const Airtable = require('airtable');
+const app = require('../server');
+
 let isProd = true;
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   // load the local .env file
-  require("dotenv").config();
+  dotenv.config();
   isProd = false;
 }
 
 const apiKey = process.env.AIRTABLE_API_KEY;
-const Airtable = require("airtable");
 const baseId = isProd
   ? process.env.AIRTABLE_PROD_PHONE_BASE
   : process.env.AIRTABLE_DEV_PHONE_BASE;
-const base = new Airtable({ apiKey: apiKey }).base(baseId);
+const base = new Airtable({ apiKey }).base(baseId);
+
+const todayAt = (hour) => {
+  const date = moment().format().slice(0, 10);
+  return moment(`${date} ${hour}`);
+};
 
 const getShiftNumbers = (timePeriod, callback) => {
+  const languageToPhones = {};
   const done = (err) => {
     if (err) {
       console.error(err);
@@ -23,17 +30,16 @@ const getShiftNumbers = (timePeriod, callback) => {
     }
     callback(languageToPhones);
   };
-  const languageToPhones = {};
   const regex = /(\(|\)|\s|-)/gi;
-  base("Volunteers")
-    .select({ view: "Grid view" })
+  base('Volunteers')
+    .select({ view: 'Grid view' })
     .eachPage((records, fetchNextPage) => {
       // This function (`page`) will get called for each page of records.
       records.forEach((record) => {
-        const unformattedPhone = record.get("Phone");
+        const unformattedPhone = record.get('Phone');
         if (!unformattedPhone) return;
 
-        const phoneNumber = `+1${unformattedPhone.replace(regex, "")}`;
+        const phoneNumber = `+1${unformattedPhone.replace(regex, '')}`;
         const languages = record.get(timePeriod);
         if (languages) {
           languages.forEach((langauge) => {
@@ -52,50 +58,45 @@ const getShiftNumbers = (timePeriod, callback) => {
       fetchNextPage();
     }, done);
 };
-
 let shiftLastGathered;
-const getInitialShift = (callback) => {
-  shiftLastGathered = moment();
-  if (moment().isBefore(todayAt(17))) {
-    getShiftNumbers(`${moment().format("dddd")} 2PM - 5PM`, callback);
-  } else {
-    getShiftNumbers(`${moment().format("dddd")} 5PM - 8PM`, callback);
-  }
-  shiftTimer();
-};
-
 const saveShiftNumbers = (languagesToPhones) => {
-  app.set("languages", languagesToPhones);
+  app.set('languages', languagesToPhones);
   shiftLastGathered = moment();
-};
-const todayAt = (hour) => {
-  const date = moment().format().slice(0, 10);
-  return moment(`${date} ${hour}`);
 };
 const shiftTimer = () => {
   // do stuff
   const now = moment();
-  const todayAt2Pm = todayAt("14");
-  const todayAt5Pm = todayAt("17");
-  const todayAt8Pm = todayAt("20");
+  const todayAt2Pm = todayAt('14');
+  const todayAt5Pm = todayAt('17');
+  const todayAt8Pm = todayAt('20');
   if (
     now.isBetween(todayAt2Pm, todayAt5Pm) &&
     !shiftLastGathered.isBetween(todayAt2Pm, todayAt5Pm)
   ) {
     // if now is between today at 2pm & today & 5pm and shiftLastGathered isn't
-    getShiftNumbers(`${now.format("dddd")} 2PM - 5PM`, saveShiftNumbers);
+    getShiftNumbers(`${now.format('dddd')} 2PM - 5PM`, saveShiftNumbers);
   } else if (
     now.isBetween(todayAt5Pm, todayAt8Pm) &&
     !shiftLastGathered.isBetween(todayAt5Pm, todayAt8Pm)
   ) {
     // if now is between today at 2pm & today & 5pm and shiftLastGathered isn't
-    getShiftNumbers(`${now.format("dddd")} 5PM - 8PM`, saveShiftNumbers);
+    getShiftNumbers(`${now.format('dddd')} 5PM - 8PM`, saveShiftNumbers);
   }
 
   // do it again in 0.5 seconds (or more)
   setTimeout(() => {
     shiftTimer();
   }, 500);
+};
+
+const getInitialShift = (callback) => {
+  shiftLastGathered = moment();
+  if (moment().isBefore(todayAt(17))) {
+    getShiftNumbers(`${moment().format('dddd')} 2PM - 5PM`, callback);
+  } else {
+    getShiftNumbers(`${moment().format('dddd')} 5PM - 8PM`, callback);
+  }
+  shiftTimer();
 };
 
 module.exports = {
